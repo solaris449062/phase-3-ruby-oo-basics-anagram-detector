b8c6416034bc86bcf5bbe4af58f5a52b7b644cca
Sungoh Park <spark449062@gmail.com>
2021-09-03 18:11:30 -0600

    commit tree fc77e3e9dcccaf3d1ec7697d4203613b160bb7ec

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Anagram
+
+    def initialize(word)
+        @word = word
+    end
+
+    def word_to_sorted_characters(word) #lexicographically sort
+        word.chars().sort
+    end
+
+    def match(array_words)
+        array_words.select do |word|
+            word_to_sorted_characters(@word) == word_to_sorted_characters(word)
+        end
+    end
+
+end
